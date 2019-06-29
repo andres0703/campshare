5d2607996bc60e658032298b0f44436226eac685
andres <andreswang0703@gmail.com>
2019-06-29 15:54:06 -0400

    web app prototype

--- app.js
+++ app.js
@@ -1,35 +1,67 @@
+
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({	extended: true }));
+mongoose.connect(keys.mongoUri, { useNewUrlParser: true });
+
+var campSchema = new mongoose.Schema({
+	  name: String,
+		image: String,
+		description: String
+});
+var camp = mongoose.model("camp", campSchema);
 
 app.get("/", function(req, res) {
     res.render("homepage");
 });
 
 app.get("/camps", function(req, res) {
-    var campgrounds = [
-        {name: "camp A", image: "https://media.wired.com/photos/599b4cfd4fa6fc733c11e30d/master/pass/iStock-820873602.jpg"},
-        {name: "camp B", image: "https://images.wilderness-safaris.com/uploads/medium/file/1723/small_focal_0903-kwetsani-camp.jpg"},
-        {name: "camp C", image: "https://images.unsplash.com/photo-1484960055659-a39d25adcb3c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},
-			  {name: "camp D", image: "https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402__340.jpg"},
-			  {name: "camp E", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0kp60ncXnIGArUpS_Nc6KA68aG7X-Bbtz0q-Djot_nT7yDa5V"}];
-
-    res.render("campgrounds", {campgrounds: campgrounds});
+    camp.find({}, function(err, allCamps) {
+			  if (err) {
+				  	console.log("Error fetching camps from database.");
+					  console.log(err);
+				} else {
+					  res.render("index", {campgrounds: allCamps});
+				}
+		})
 });
 
 app.post("/camps", function(req, res) {
     var name = req.body.name;
     var imageUrl = req.body.image;
+		var desc = req.body.description;
 
-    var newcamp = {name: name, image: imageUrl}
-    res.render("newCampAdded", {newcamp: newcamp});
+    var newcamp = {name: name, image: imageUrl, description: desc};
+		if (imageUrl != null) {
+				camp.create(newcamp, function(err, newlyCreated) {
+				    if (err) {
+							  console.log("Error creating database record: ");
+						    console.log(err);
+						} else {
+					      console.log("a new camp created");
+						    res.redirect("/camps");
+						}
+				});
+		};
 });
 
 app.get("/camps/new", function(req, res) {
     res.render("newcamp");
+});
+
+app.get("/camps/:id", function(req, res) {
+    camp.findById(req.params.id, function(err, foundCamp) {
+			  if (err) {
+					  console.log(err);
+				} else {
+					  res.render("show", {camp: foundCamp });
+				}
+		})
 });
 
 app.listen(process.env.PORT || 5000, process.env.IP, function() {
