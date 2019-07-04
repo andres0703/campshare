91299ffd70ee673bfb62cef628e1a5141714c615
andres <andreswang0703@gmail.com>
2019-07-04 15:04:41 -0400

    standard formatted code

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
 
-const express    = require("express");
-const app        = express();
-const mongoose   = require("mongoose");
-const passport   = require("passport");
-const bodyParser = require("body-parser");
-const LocalStrategy = require("passport-local");
-const Camp       = require("./models/camp");
-const Comment    = require("./models/comment");
-const User       = require("./models/user");
-const seedDB     = require("./seed.js");
-const commentRoutes = require("./routes/comments");
-const campRoutes    = require("./routes/camps");
-const indexRoutes   = require("./routes/index");
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const passport = require('passport')
+const bodyParser = require('body-parser')
+const LocalStrategy = require('passport-local')
+const Camp = require('./models/camp')
+const Comment = require('./models/comment')
+const User = require('./models/user')
+const seedDB = require('./seed.js')
+const commentRoutes = require('./routes/comments')
+const campRoutes = require('./routes/camps')
+const indexRoutes = require('./routes/index')
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({	extended: true }));
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({	extended: true }))
 
 // seed the database
-seedDB();
+seedDB()
 
 // passport configuration
-app.use(require("express-session")({
-  secret: "abcde",
+app.use(require('express-session')({
+  secret: 'abcde',
   resave: false,
   saveUnitialize: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 // middleware
-app.use(function(req, res, next) {
-  res.locals.curUser = req.user;
-  next();
-});
+app.use(function (req, res, next) {
+  res.locals.curUser = req.user
+  next()
+})
 
 // routing
-app.use("/", indexRoutes);
-app.use("/camps", campRoutes);
-app.use("/camps/:id/comments", commentRoutes);
+app.use('/', indexRoutes)
+app.use('/camps', campRoutes)
+app.use('/camps/:id/comments', commentRoutes)
 
-
-app.listen(process.env.PORT || 3000, process.env.IP, function() {
-    console.log("app started");
-});
+app.listen(process.env.PORT || 3000, process.env.IP, function () {
+  console.log('app started')
+})
--- routes/camps.js
+++ routes/camps.js
@@ -1,50 +1,53 @@
-const express = require("express");
-const router  = express.Router({mergeParams: true});
-const Camp    = require("../models/camp");
-const Comment = require("../models/comment");
+const express = require('express')
+const router = express.Router({mergeParams: true})
+const Camp = require('../models/camp')
+const Comment = require('../models/comment')
 
-router.get("/", function(req, res) {
-    Camp.find({}, function(err, allCamps) {
+router.get('/', function (req, res) {
+  Camp.find({}, function (err, allCamps) {
 			  if (err) {
-				  	console.log("Error fetching camps from database.");
-					  console.log(err);
-				} else {
-					  res.render("camps/index", {campgrounds: allCamps});
-				}
-		})
-});
+				  	console.log('Error fetching camps from database.')
+					  console.log(err)
+  } else {
+    allCamps.forEach(function (camp) {
+      console.log(camp.id)
+    })
+					  res.render('camps/index', {campgrounds: allCamps})
+  }
+  })
+})
 
-router.post("/", function(req, res) {
-    var name = req.body.name;
-    var imageUrl = req.body.image;
-		var desc = req.body.description;
+router.post('/', function (req, res) {
+  var name = req.body.name
+  var imageUrl = req.body.image
+  var desc = req.body.description
 
-    var newcamp = {name: name, image: imageUrl, description: desc};
-		if (imageUrl != null) {
-				Camp.create(newcamp, function(err, newlyCreated) {
+  var newcamp = {name: name, image: imageUrl, description: desc}
+  if (imageUrl != null) {
+    Camp.create(newcamp, function (err, newlyCreated) {
 				    if (err) {
-							  console.log("Error creating database record: ");
-						    console.log(err);
-						} else {
-					      console.log("a new camp created");
-						    res.redirect("/camps");
-						}
-				});
-		};
-});
+							  console.log('Error creating database record: ')
+						    console.log(err)
+    } else {
+					      console.log('a new camp created')
+						    res.redirect('/camps')
+    }
+    })
+  };
+})
 
-router.get("/new", function(req, res) {
-    res.render("camps/newcamp");
-});
+router.get('/camps/new', function (req, res) {
+  res.render('camps/newcamp')
+})
 
-router.get("/:id", function(req, res) {
-    Camp.findById(req.params.id).populate("comments").exec(function(err, foundCamp) {
+router.get('/:id', function (req, res) {
+  Camp.findById(req.params.id).populate('comments').exec(function (err, foundCamp) {
 			  if (err) {
-					  console.log(err);
-				} else {
-            res.render("camps/show", {campground: foundCamp });
-				}
-		})
-});
+					  console.log(err)
+  } else {
+    res.render('camps/show', {campground: foundCamp })
+  }
+  })
+})
 
-module.exports = router;
+module.exports = router
